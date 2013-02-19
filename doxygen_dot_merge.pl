1daa19f07be14346be7e21f93fb3c6ef42367bd2
bharanis <bharanis78@gmail.com>
2013-02-19 15:45:22 -0800

    Update doxygen_dot_merge.pl
    1) main() fn enumerator ## commented out for now
    2) take care of icgraph and cgraph files passed as parameters together. This is done by looking for "dir=back" which would be set for icgraph files. In that case, we swap the nodes.
    3) Generate the dot file @ html/merged.dot
    4) generate the png and map files
    5) generate a container html file that loads the png and map files.

--- doxygen_dot_merge.pl
+++ doxygen_dot_merge.pl
@@ -10,6 +10,7 @@ my %sym2node = ();
 my %sym2url = ();
 my %file2nodes = ();
 my %node2sym = ();
+my $main_fn_index = 1;
 
 
 
@@ -26,6 +27,12 @@ foreach $argnum (0 .. $#ARGV) {
     if (/Node/) {
       my ($num, $sym, $rest) = /^ *Node(\d+) \[label=\"([^\"]+)"(.*)$/;
       if ($num) {
+         if ($sym eq "main") {
+             $sym = $sym.$main_fn_index;
+             # if your code has many test files each having a main() function, this will give each main fn a nerw node.
+             # commented out for now.
+             #$main_fn_index ++;
+         }
          if (exists $sym2node{$sym}) {
             $node2node{$num} = $sym2node{$sym};
          } else {
@@ -48,10 +55,16 @@ foreach $argnum (0 .. $#ARGV) {
 
   # pass 2:: renaming
   open (dotf,$ARGV[$argnum]) || die "Cannot read dot file!\n";;
+  
   while (<dotf>) {
     if (/Node/) {
       my ($num1, $num2, $rest) = /^ *Node(\d+) -> Node(\d+)(.*)$/;
       if ($num1) {
+         if ($rest =~ m/dir=back/) {
+             my $t = $num1;
+             $num1 = $num2;
+             $num2 = $t;
+         }
          my $key=$node2node{$num1}.".".$node2node{$num2};
          if (exists $connections{$key}) {
          } else {
@@ -80,16 +93,24 @@ for (sort keys %sym2node) {
 }
 for (sort keys %connections) {
     my ($n1,$n2) = /(\d+)\.(\d+)/;
+    #next if ($node2sym{$n1} =~ m/jbuf_/);
+    #next if ($node2sym{$n2} =~ m/jbuf_/);
+    next if (($node2sym{$n1} =~ m/jbuf_/) && ($node2sym{$n2} =~ m/jbuf_/));
+
     print df "   Node".$n1." -> Node".$n2." [dir=front,color=\"midnightblue\",fontsize=\"10\",style=\"solid\",fontname=\"FreeSans\"];\n";
 }
 
+#say Dumper(%file2nodes);
+
 for (sort keys %file2nodes) {
   print df "#file >> ".$_."\n";
-  print df "{ rank=same; ";
-  foreach $i (0 .. (@{$file2nodes{$_}}-1)) {
-    print df "   Node".$file2nodes{$_}[$i]."; ";
+  if ((@{$file2nodes{$_}}-1) < 6) {
+    print df "{ rank=same; ";
+    foreach $i (0 .. (@{$file2nodes{$_}}-1)) {
+      print df "   Node".$file2nodes{$_}[$i]."; ";
+    }
+    print df "}\n";
   }
-  print df "}\n";
 }
 
 
